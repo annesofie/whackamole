653a11935bc51043019184631eb1fe8438d463c5
Ruben Schmidt Mällberg <rubschmi@gmail.com>
2016-04-06 12:33:46 +0200

    sockets are now up and running

--- core/src/com/whackamole/game/GameClass.java
+++ core/src/com/whackamole/game/GameClass.java
@@ -2,26 +2,75 @@ package com.whackamole.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.whackamole.game.utils.Constants;
+import com.whackamole.game.utils.SocketRetreiver;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 public class GameClass extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch batch;
+    Texture img;
+    Socket socket;
+    JsonReader reader;
+    int imagex;
+    int imagey;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+        imagex = 0;
+        imagey = 0;
+
+        reader = new JsonReader();
+        SocketRetreiver retreiver = new SocketRetreiver();
+        socket = retreiver.getSocket();
+        socket.on(Socket.EVENT_CONNECT_ERROR, onConnectError);
+        socket.on(Socket.EVENT_CONNECT_TIMEOUT, onConnectError);
+        socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {
+                System.out.println("connected to socket");
+            }
+
+        });
+        socket.on("chat message", onNewMessage);
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, imagex, imagey);
+        batch.end();
+    }
+
+    private Emitter.Listener onConnectError = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            System.out.println("Connected error");
+        }
+    };
+
+    private Emitter.Listener onNewMessage = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            String message = (String) args[0];
+            String[] coord = message.split(",");
+            if (coord.length < 2){
+                return;
+            }
+            imagex = Integer.parseInt(coord[0]);
+            imagey = Integer.parseInt(coord[1]);
+        }
+    };
 }
