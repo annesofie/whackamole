a197056b982bc24f9d812f991d47af33563c4902
Anne Sofie Strand Erichsen <AnneSofie@dhcp-10-22-29-200.wlan.ntnu.no>
2016-04-07 14:24:33 +0200

    remove unneccesary kode

--- core/src/com/whackamole/game/model/MainMenuState.java
+++ core/src/com/whackamole/game/model/MainMenuState.java
@@ -25,15 +25,11 @@ public class MainMenuState {
      *
      *
      */
-
     private GameSettings game_setting;
     private String name;
     private int id;
     private GameSettings.gameTheme game_theme;
     private int num_of_moles;
 
-    public LobbyState(){
-        game_setting = new GameSettings(name, id, game_theme, num_of_moles);
-    }
 
 }
