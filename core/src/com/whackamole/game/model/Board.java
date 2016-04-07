1371a8702f0f75a055e83dfdc046ebcd0bbc34d2
Øystein Hauan <oysteibh@stud.ntnu.no>
2016-04-07 09:14:47 +0200

    initial User and Board(?)

--- core/src/com/whackamole/game/model/Board.java
+++ core/src/com/whackamole/game/model/Board.java
@@ -1,7 +1,68 @@
 package com.whackamole.game.model;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by AnneSofie on 04.04.2016.
  */
 public class Board {
+
+    Array grid;
+    List<Mole> moles;
+    private Texture background;
+
+    public enum Theme{
+        Kardashian, President
+    }
+
+    public Board(Theme theme){
+
+        grid = new Array();
+        moles = new ArrayList<Mole>();
+        //Choose correct background for current theme
+        background = new Texture(Gdx.files.internal("background.png"));
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                grid.add(new Mole(new Vector2(i, j)), theme);
+
+            }
+        }
+
+
+    }
+
+    public void setMole(Mole mole){
+
+    }
+
+    public int getX(){
+
+    }
+
+    public int getY(){
+
+    }
+
+    public boolean isMole(){
+
+    }
+
+    public void hideMole(){
+
+    }
+
+    public void showMole(){
+
+    }
+
+
+
+
 }
--- core/src/com/whackamole/game/model/User.java
+++ core/src/com/whackamole/game/model/User.java
@@ -1,7 +1,66 @@
 package com.whackamole.game.model;
 
+import java.util.ArrayList;
+
 /**
  * Created by AnneSofie on 04.04.2016.
  */
 public class User {
+
+    String username;
+    int userId;
+    boolean loggedIn = false;
+    public static ArrayList<User> userList = new ArrayList<User>();
+    public static ArrayList<Integer> idList = new ArrayList<Integer>();
+    public static ArrayList<String> namelist = new ArrayList<String>();
+
+    public User(String username){
+        this.username = setUsername(username);
+        this.userId = createId();
+        this.loggedIn = true;
+        userList.add(this);
+        idList.add(this.userId);
+        namelist.add(this.username);
+        
+    }
+
+    private int createId(){
+        if(idList.size() > 0) {
+            return idList.get(idList.size() - 1) + 1;
+        }
+        return 0;
+    }
+
+    public String setUsername(String username){
+        for (String s: namelist) {
+            if(s.equalsIgnoreCase(username)){
+                throw new IllegalArgumentException("Username is already taken.");
+            }
+        }
+        return username;
+    }
+
+    //belongs in controller maybe?
+    public void logout(){
+        int temp = userList.indexOf(this);
+        userList.remove(this);
+        synchronized (idList) {
+            if (idList.get(temp) == this.userId) {
+                idList.remove(temp);
+            }
+        }
+        namelist.remove(this.username);
+        //probably unnecessary
+        this.loggedIn = false;
+    }
+
+    public String getName(){
+        return this.username;
+    }
+
+    public int getId(){
+        return this.userId;
+    }
+
+    
 }
