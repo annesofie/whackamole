7045a35f6f0993b871de3186c86ac81b408aecfd
Øystein Hauan <oysteibh@stud.ntnu.no>
2016-04-08 16:07:18 +0200

    Not complete

--- core/src/com/whackamole/game/GameClass.java
+++ core/src/com/whackamole/game/GameClass.java
@@ -16,137 +16,137 @@ import io.socket.emitter.Emitter;
 public class GameClass extends Game {
 
 
-	/**
-	 *
-	 * 	Rendering is happening here.
-	 * 	This class should probably be notified by the models when changes happen there and rerender
-	 * 	based on that notification.
-	 *
-	 *
-	 */
-
-    SpriteBatch batch;
-    Texture img;
-    Socket socket;
-    JsonReader reader;
-    int imagex;
-    int imagey;
-
-    @Override
-    public void create() {
-
-        /**
-         *
-         *  RUN ON APP START
-         *  USED TO SETUP EVERYTHING
-         *
-         *
-         */
-
-
-
-        batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
-        imagex = 0;
-        imagey = 0;
-
-        reader = new JsonReader();
-        SocketRetreiver retreiver = SocketRetreiver.getInstance();
-        socket = retreiver.getSocket();
-        socket.on(Socket.EVENT_CONNECT_ERROR, onConnectError);
-        socket.on(Socket.EVENT_CONNECT_TIMEOUT, onConnectError);
-        socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
-
-            @Override
-            public void call(Object... args) {
-                System.out.println("connected to socket");
-            }
-
-        });
-        socket.on("chat message", onNewMessage);
-        socket.on("new mole", newMole);
-    }
-
-    @Override
-    public void render() {
-        Board board = new Board(Theme.KARDASHIAN);
-        board.setMole(board.getMoles().get(4));
-        BoardRenderer renderer = new BoardRenderer(board);
-        renderer.render();
-        //board.getMoles().get(6).hide();
-        //MoleRenderer mr = new MoleRenderer(board.getMoles().get(6));
-        //mr.render();
-
-
-       /* batch.begingit add();
-        batch.draw(img, imagex, imagey);
-        batch.end();*/
-    }
-
-    @Override
-    public void pause() {
-        /**
-         *  This method is run when a user exits the app.
-         *  It pauses the game.
-         *
-         */
-    }
-
-
-    @Override
-    public void resume() {
-
-        /**
-         *  This method is called when the app is put back into the foreground.
-         *  It is run to resume the game.
-         *
-         */
-    }
-
-    @Override
-    public void dispose() {
-        /**
-         *  This method is called when the application is closed (the process itself).
-         *  This should be used to do some cleanup (save states, etc. if necessary)
-         *
-         */
-
-
-    }
-
-
-
-    private Emitter.Listener onConnectError = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            System.out.println("Connected error");
-        }
-    };
-
-    private Emitter.Listener onNewMessage = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            String message = (String) args[0];
-            String[] coord = message.split(",");
-            if (coord.length < 2){
-                return;
-            }
-            imagex = Integer.parseInt(coord[0]);
-            imagey = Integer.parseInt(coord[1]);
-        }
-    };
-
-    private Emitter.Listener newMole = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            String message = (String) args[0];
-            String[] coord = message.split(",");
-            if (coord.length < 2){
-                return;
-            }
-            imagex = Integer.parseInt(coord[0]);
-            imagey = Integer.parseInt(coord[1]);
-        }
-    };
+//	/**
+//	 *
+//	 * 	Rendering is happening here.
+//	 * 	This class should probably be notified by the models when changes happen there and rerender
+//	 * 	based on that notification.
+//	 *
+//	 *
+//	 */
+//
+//    SpriteBatch batch;
+//    Texture img;
+//    Socket socket;
+//    JsonReader reader;
+//    int imagex;
+//    int imagey;
+//
+//    @Override
+        public void create() {}
+//
+//        /**
+//         *
+//         *  RUN ON APP START
+//         *  USED TO SETUP EVERYTHING
+//         *
+//         *
+//         */
+//
+//
+//
+//        batch = new SpriteBatch();
+//        img = new Texture("badlogic.jpg");
+//        imagex = 0;
+//        imagey = 0;
+//
+//        reader = new JsonReader();
+//        SocketRetreiver retreiver = SocketRetreiver.getInstance();
+//        socket = retreiver.getSocket();
+//        socket.on(Socket.EVENT_CONNECT_ERROR, onConnectError);
+//        socket.on(Socket.EVENT_CONNECT_TIMEOUT, onConnectError);
+//        socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+//
+//            @Override
+//            public void call(Object... args) {
+//                System.out.println("connected to socket");
+//            }
+//
+//        });
+//        socket.on("chat message", onNewMessage);
+//        socket.on("new mole", newMole);
+//    }
+//
+//    @Override
+//    public void render() {
+//        Board board = new Board(Theme.KARDASHIAN);
+//        board.setMole(board.getMoles().get(4));
+//        BoardRenderer renderer = new BoardRenderer(board);
+//        renderer.render();
+//        //board.getMoles().get(6).hide();
+//        //MoleRenderer mr = new MoleRenderer(board.getMoles().get(6));
+//        //mr.render();
+//
+//
+//       /* batch.begingit add();
+//        batch.draw(img, imagex, imagey);
+//        batch.end();*/
+//    }
+//
+//    @Override
+//    public void pause() {
+//        /**
+//         *  This method is run when a user exits the app.
+//         *  It pauses the game.
+//         *
+//         */
+//    }
+//
+//
+//    @Override
+//    public void resume() {
+//
+//        /**
+//         *  This method is called when the app is put back into the foreground.
+//         *  It is run to resume the game.
+//         *
+//         */
+//    }
+//
+//    @Override
+//    public void dispose() {
+//        /**
+//         *  This method is called when the application is closed (the process itself).
+//         *  This should be used to do some cleanup (save states, etc. if necessary)
+//         *
+//         */
+//
+//
+//    }
+//
+//
+//
+//    private Emitter.Listener onConnectError = new Emitter.Listener() {
+//        @Override
+//        public void call(Object... args) {
+//            System.out.println("Connected error");
+//        }
+//    };
+//
+//    private Emitter.Listener onNewMessage = new Emitter.Listener() {
+//        @Override
+//        public void call(Object... args) {
+//            String message = (String) args[0];
+//            String[] coord = message.split(",");
+//            if (coord.length < 2){
+//                return;
+//            }
+//            imagex = Integer.parseInt(coord[0]);
+//            imagey = Integer.parseInt(coord[1]);
+//        }
+//    };
+//
+//    private Emitter.Listener newMole = new Emitter.Listener() {
+//        @Override
+//        public void call(Object... args) {
+//            String message = (String) args[0];
+//            String[] coord = message.split(",");
+//            if (coord.length < 2){
+//                return;
+//            }
+//            imagex = Integer.parseInt(coord[0]);
+//            imagey = Integer.parseInt(coord[1]);
+//        }
+//    };
 
 }
--- core/src/com/whackamole/game/model/Board.java
+++ core/src/com/whackamole/game/model/Board.java
@@ -11,33 +11,49 @@ import com.badlogic.gdx.utils.Array;
  */
 public class Board {
 
-    private Array<Mole> grid = new Array<Mole>();
-    private Texture background;
+    private Array<Mole> grid = new Array<Mole>(), currentMoles = new Array<Mole>();
+    private Texture b1, b2, b3, b4, hs;
     int height, width;
     Theme theme;
+    private String filepath;
 
     public Board(Theme theme){
 
-       // moles = new ArrayList<Mole>();
-        //Choose correct background for current theme
-        //background = new Texture(Gdx.files.internal("background.png"));
         this.height = Gdx.graphics.getHeight();
         this.width = Gdx.graphics.getWidth();
         this.theme = theme;
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                grid.add(new Mole(new Vector2((2+3*i)*width/10  - 17*width/120, (3 + 3*j)*height/16 - height/32), theme, grid.size));
-               // grid.get(i+j).setMoleImg(new Texture(Gdx.files.internal("hull.png")));
-
+                grid.add(new Mole(new Vector2((2+3*i)*width/10  - 17*width/120, (3 + 3*j)*height/16 - height/32),
+                        theme, grid.size));
             }
         }
-
+        this.filepath = theme.path();
 
     }
 
-    public void setMole(Mole mole){
+    public void addCurrentMole(Mole mole){
+        currentMoles.add(mole);
+    }
 
+    public Array<Mole> getCurrentMoles(){
+        return currentMoles;
+    }
+
+    public String getPath(){
+        return this.filepath;
+    }
+
+    public void showMole(Mole mole){
+        if(mole.hidden()) {
+            mole.show();
+        }
+    }
+    public void hideMole(Mole mole){
+        if(!mole.hidden()) {
+            mole.hide();
+        }
     }
 
     public Mole getMole() {
@@ -46,11 +62,6 @@ public class Board {
 
     public Array<Mole> getMoles(){
         return grid;
-    }
-
-    public int getX(){
-        return 0;
-
     }
 
     public int getY(){
