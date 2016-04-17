dd2160b256d034729019c36e97e1a1cd154c4993
larskraa <larskraa@stud.ntnu.no>
2016-04-17 03:05:21 +0200

    quick fix

--- core/src/com/whackamole/game/utils/Constants.java
+++ core/src/com/whackamole/game/utils/Constants.java
@@ -6,8 +6,9 @@ import com.whackamole.game.model.Theme;
  * Created by rubenschmidt on 06.04.2016.
  */
 public class Constants {
-    //public static final String SERVER_URL = "https://murmuring-everglades-2479.herokuapp.com/";
-    public static final String SERVER_URL = "http://10.0.2.2:5000";
+    public static final String SERVER_URL = "https://murmuring-everglades-2479.herokuapp.com/";
+    //public static final String SERVER_URL = "https://agile-sands-28562.herokuapp.com/";
+    //public static final String SERVER_URL = "http://10.0.2.2:5000";
 
     public static final float hitBoxRelativeHeight = (float) 33/160;
     public static final float hitBoxRelativeWidth = (float) 260/900;
