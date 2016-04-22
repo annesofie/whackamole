b9672c2d6794b54b95ed08df1a618dbe20b111e8
larskraa <larskraa@stud.ntnu.no>
2016-04-22 16:38:06 +0200

    server url change

--- core/src/com/whackamole/game/utils/Constants.java
+++ core/src/com/whackamole/game/utils/Constants.java
@@ -7,10 +7,9 @@ import com.whackamole.game.model.Theme;
  */
 public class Constants {
     //public static final String SERVER_URL = "https://murmuring-everglades-2479.herokuapp.com/";
-    //public static final String SERVER_URL = "https://agile-sands-28562.herokuapp.com/";
-    public static final String SERVER_URL = "http://10.22.23.95:5000";
-
-
+    public static final String SERVER_URL = "https://agile-sands-28562.herokuapp.com/";
+    //public static final String SERVER_URL = "http://10.22.23.95:5000";
+    
     public static final float hitBoxRelativeHeight = (float) 33/160;
     public static final float hitBoxRelativeWidth = (float) 260/900;
     public static final float buttonHeightWidthRatio = (float) 154/692;
